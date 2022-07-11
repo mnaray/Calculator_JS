c6787d6d7b085c5cd0a2c50e92e9c73692d5084f
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-07-11 21:30:41 +0200

    add calculator class
    Here I construct the actual calculator object with all it's methods.

--- script.js
+++ script.js
@@ -1,7 +1,49 @@
+class Calculator {
+    constructor(typed, typing) {
+        this.typed = typed
+        this.typing = typing
+        this.clear()
+    }
+
+    clear() {
+        this.typing = ""
+        this.typed = ""
+        this.operation = undefined
+    }
+
+    delete() {
+
+    }
+
+    appendNumber(number) {
+
+    }
+
+    chooseOperation(operation) {
+
+    }
+
+    compute() {
+
+    }
+
+    getDisplayedNumber(number) {
+
+    }
+
+    updateDisplay() {
+
+    }
+}
+
 const numberButtons = document.querySelectorAll("[number]")
 const operationButtons = document.querySelectorAll("[operation]")
+const equalsButton = document.querySelector("[equals]")
 const clearButton = document.querySelector("[all-clear]")
 const deleteButton = document.querySelector("[delete]")
-const equalsButton = document.querySelector("[equals]")
 const typed = document.querySelector("[typed]")
 const typing = document.querySelector("[typing]")
+
+const calculator = new Calculator(typed, typing)
+
+numberButtons.forEach(button => {

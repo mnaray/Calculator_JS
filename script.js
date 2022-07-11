16fd3b82f0fcfdcfec400bd738235f2bb5e97b93
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-07-11 20:40:12 +0200

    adjust and query buttons
    In this commit I query all of the buttons of the calculator and assign them to variables. this way I can handle them like objects.

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll("[number]")
+const operationButtons = document.querySelectorAll("[operation]")
+const clearButton = document.querySelector("[all-clear]")
+const deleteButton = document.querySelector("[delete]")
+const equalsButton = document.querySelector("[equals]")
+const typed = document.querySelector("[typed]")
+const typing = document.querySelector("[typing]")
